ba34e403f03e9ec962da13c0e3b541b8fa4078a5
Nick Romney <26225537+nickromney@users.noreply.github.com>
2024-11-15 11:46:39 +0000

    chore: update stow for amethyst and bat

--- stow.sh
+++ stow.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-if ! command -v stow &> /dev/null; then
+if ! command -v stow &>/dev/null; then
   echo "stow is not installed. Please install stow first."
   echo "Check instructions at https://www.gnu.org/software/stow/"
   exit 1
@@ -9,7 +9,7 @@ fi
 
 stow stow
 
-stow_array=( gh git karabiner kitty nvim ssh starship tmux zsh)
+stow_array=(amethyst bat gh git karabiner kitty nvim ssh starship tmux zsh)
 
 for dir in "${stow_array[@]}"; do
   if stow -v -R -t "$HOME" "$dir"; then
