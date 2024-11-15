d31a96937eeda15906e68ab01322e4744ccebe8b
Nick Romney <26225537+nickromney@users.noreply.github.com>
2024-11-15 09:13:19 +0000

    feat: add stow and brew configurations

---
+++ stow.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -euo pipefail
+
+if ! command -v stow &> /dev/null; then
+  echo "stow is not installed. Please install stow first."
+  echo "Check instructions at https://www.gnu.org/software/stow/"
+  exit 1
+fi
+
+stow stow
+
+stow_array=( gh git karabiner kitty nvim ssh starship tmux zsh)
+
+for dir in "${stow_array[@]}"; do
+  if stow -v -R -t "$HOME" "$dir"; then
+    echo "Stowed $dir"
+  else
+    echo "Error stowing $dir"
+  fi
+done
